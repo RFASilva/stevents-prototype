fc26b8d8b98a8c33a21aae0ae35476e3e508259d
Ricardo Silva <ricardofcsasilva@gmail.com>
2015-08-14 16:58:45 +0100

    database connection

--- src/main/java/core/load_data/DataStoreInfo.java
+++ src/main/java/core/load_data/DataStoreInfo.java
@@ -1,7 +1,10 @@
 package core.load_data;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import core.Config;
 
@@ -9,18 +12,37 @@ import core.Config;
 public class DataStoreInfo {
 
 	public static Connection getMetaStore() {
-		return getConnection(Config.getConfigString("meta_store_url"));
+//		return getConnection(Config.getConfigString("meta_store_url"));
+		return getConnection();
 	}
 	
-	private static Connection getConnection(final String url) {
-		Connection connection = null;
+	private static Connection getConnection()  {
+	    URI dbUri;
 		try {
-			Class.forName("org.postgresql.Driver");
-			connection = DriverManager.getConnection(url);
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		}
+			dbUri = new URI(System.getenv("DATABASE_URL"));
+		
 
-		return connection;
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+	    return DriverManager.getConnection(dbUrl, username, password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	  
 	}
+	
+//	private static Connection getConnection(final String url) {
+//		Connection connection = null;
+//		try {
+//			Class.forName("org.postgresql.Driver");
+//			connection = DriverManager.getConnection(url);
+//		} catch (Exception exception) {
+//			exception.printStackTrace();
+//		}
+//
+//		return connection;
+//	}
 }
